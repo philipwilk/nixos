cfc82d27713a1063f7110fa68d037baf45a4c6b5
Philip Wilk <wiryfuture@gmail.com>
2023-08-31 16:32:47 +0100

    add keychron udev and modify huion udev to work for all of their tablets

--- configs/workstation.nix
+++ configs/workstation.nix
@@ -138,9 +138,10 @@
     openssh.settings.extraConfig = "+PubkeyAuthOptions verify-required+";
     udev.extraRules = ''
       KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
-      SUBSYSTEM=="hidraw", ATTRS{idVendor}=="256c", ATTRS{idProduct}=="0061", MODE="0666"
+      SUBSYSTEM=="hidraw*", ATTRS{idVendor}=="256c", MODE="0666"
       SUBSYSTEM=="usb", ATTRS{idVendor}=="256c", ATTRS{idProduct}=="0061", MODE="0666"
       SUBSYSTEM=="input", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="0094", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+      KERNEL=="hidraw*", ATTRS{idVendor}=="3434", MODE="0666"
     '';
   };
 
