de5fad15f50734f4b54ff0b899032f6d821579d0
Philip Wilk <wiryfuture@gmail.com>
2023-09-16 18:16:13 +0100

    add huion support when connected over a pcie usb controller

--- configs/workstation.nix
+++ configs/workstation.nix
@@ -141,6 +141,7 @@
       KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput"
       SUBSYSTEM=="hidraw*", ATTRS{idVendor}=="256c", MODE="0666"
       SUBSYSTEM=="usb", ATTRS{idVendor}=="256c", ATTRS{idProduct}=="0061", MODE="0666"
+      SUBSYSTEM=="pci", ATTRS{idVendor}=="256c", ATTRS{idProduct}=="0061", MODE="0666"
       SUBSYSTEM=="input", ATTRS{idVendor}=="28bd", ATTRS{idProduct}=="0094", ENV{LIBINPUT_IGNORE_DEVICE}="1"
       KERNEL=="hidraw*", ATTRS{idVendor}=="3434", MODE="0666"
     '';
