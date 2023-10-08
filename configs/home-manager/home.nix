0cae14d8911a6bd6a20b8e01b433d1119ace6cb3
Philip Wilk <p.wilk@student.reading.ac.uk>
2023-10-08 21:18:47 +0100

    add git to hm

--- configs/home-manager/home.nix
+++ configs/home-manager/home.nix
@@ -4,11 +4,15 @@
   ...
 }:
 {
+  imports = [ programs/git.nix ];
+    
   home = {
     username = "philip";
     homeDirectory = "/home/philip";
     stateVersion = "23.05";
   };
 
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+  };
 }
---
+++ configs/home-manager/programs/git.nix
@@ -0,0 +1,41 @@
+{
+	pkgs,
+	lib,
+	...
+}:
+{
+	programs.git = {
+    enable = true;
+    userName = "Philip Wilk";
+    userEmail = "p.wilk@student.reading.ac.uk";
+    aliases = {
+      pl = "log --graph --abbrev-commit --decorate --stat";
+    };
+    diff-so-fancy.enable = true;
+		signing = {
+			signByDefault = true;
+			key = "/home/philip/.ssh/gitKey";
+		};
+		extraConfig = {
+			core = {
+				editor = "hx";
+				sshCommand = "ssh -i /home/philip/.ssh/gitKey";
+			};
+			gpg = {
+				format = "ssh";
+			};
+			pull = {
+				rebase = true;
+			};
+			push = {
+				autoSetupRemote = true;
+			};
+			ssh = {
+				identity = "/home/philip/.ssh/gitKey";
+			};
+			init = {
+				defaultBranch = "main";
+			};
+		};
+  };
+}
