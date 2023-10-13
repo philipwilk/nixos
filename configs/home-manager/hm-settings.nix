7a7198190ff31a32cecd1e3c8227da7481bf611b
Philip Wilk <p.wilk@student.reading.ac.uk>
2023-10-13 22:44:44 +0100

    catppuccin

--- configs/home-manager/hm-settings.nix
+++ configs/home-manager/hm-settings.nix
@@ -1,11 +1,18 @@
 { pkgs
 , lib
+, catppuccin
 , ...
 }:
 {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.philip = { imports = [ ./home.nix ]; };
+    users.philip = {
+      imports = [
+        ./home.nix
+        catppuccin.homeManagerModules.catppuccin
+        ./programs/catppuccin.nix
+      ];
+    };
   };
 }
---
+++ configs/home-manager/programs/catppuccin.nix
@@ -0,0 +1,22 @@
+{ pkgs
+, lib
+, ...
+}:
+{
+  catppuccin.flavour = "latte";
+  gtk.catppuccin.enable = true;
+  programs = {
+    starship = {
+      enable = true;
+      catppuccin.enable = true;
+    };
+    helix = {
+      enable = true;
+      catppuccin.enable = true;
+    };
+    kitty = {
+      enable = true;
+      catppuccin.enable = true;
+    };
+  };
+}
