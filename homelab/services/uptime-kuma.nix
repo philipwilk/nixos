d806ee6a880045259b9d50c7d3cc57820cc8a92a
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-02-05 15:38:14 +0000

    enable uptime kuma

---
+++ homelab/services/uptime-kuma.nix
@@ -0,0 +1,19 @@
+{
+	config,
+	lib,
+	...
+}: 
+{
+	config = lib.mkIf config.homelab.services.uptime-kuma.enable {
+		services.uptime-kuma = {
+			enable = true;
+			appriseSupport = true;
+			settings = {
+				HOST = "0.0.0.0";
+			};
+		};
+
+		# The default port is 3001 (there is no option for it)
+    networking.firewall.interfaces."eno1".allowedTCPPorts = [ 3001 ];
+	};
+}
