6b448a2c30a7fb69a07d06fb5cd9ad2c0beb0ada
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-05-06 17:52:27 +0100

    nextcloud: use upstream's cache options

--- homelab/services/nextcloud.nix
+++ homelab/services/nextcloud.nix
@@ -14,6 +14,7 @@
     services.nextcloud = {
       enable = true;
       https = true;
+      nginx.recommendedHttpHeaders = true;
       autoUpdateApps.enable = true;
       maxUploadSize = "4096M";
       enableImagemagick = true;
@@ -48,9 +49,48 @@
       };
     };
 
+    services.nginx.appendHttpConfig = ''
+      map $arg_v $asset_immutable {
+          "" "";
+          default ", immutable";
+      }
+    '';
     services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
       forceSSL = true;
       enableACME = true;
+      extraConfig = ''
+        index index.php index.html /index.php$request_uri;
+          ${lib.strings.optionalString (config.services.nextcloud.nginx.recommendedHttpHeaders) ''
+            add_header Cache-Control                     "public, max-age=15778463$asset_immutable";
+            add_header X-Content-Type-Options nosniff;
+            add_header X-XSS-Protection "1; mode=block";
+            add_header X-Robots-Tag "noindex, nofollow";
+            add_header X-Download-Options noopen;
+            add_header X-Permitted-Cross-Domain-Policies none;
+            add_header X-Frame-Options sameorigin;
+            add_header Referrer-Policy no-referrer;
+          ''}
+          ${lib.strings.optionalString (config.services.nextcloud.https) ''
+            add_header Strict-Transport-Security "max-age=${toString config.services.nextcloud.nginx.hstsMaxAge}; includeSubDomains" always;
+          ''}
+      '';
+      locations = {
+        "~ \\.(?:css|js|mjs|svg|gif|png|jpg|jpeg|ico|wasm|tflite|map|html|ttf|bcmap|mp4|webm|ogg|flac)$".extraConfig = lib.mkForce ''
+          try_files $uri /index.php$request_uri;
+          access_log off;
+          location ~ \.mjs$ {
+            default_type text/javascript;
+          }
+          location ~ \.wasm$ {
+            default_type application/wasm;
+          }
+        '';
+        "~ \.woff2?$".extraConfig = ''
+          try_files $uri /index.php$request_uri;
+          expires 7d;
+          access_log off;
+        '';
+      };
     };
 
     # ensure that mariadb is running *before* running the setup
