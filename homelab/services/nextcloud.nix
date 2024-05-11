5a795f3428dbcad0ddd36e50ef639adefca000aa
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-05-12 00:05:38 +0100

    nextcloud: restore most of 6b448a2c30a7

--- homelab/services/nextcloud.nix
+++ homelab/services/nextcloud.nix
@@ -49,31 +49,9 @@
       };
     };
 
-    services.nginx.appendHttpConfig = ''
-      map $arg_v $asset_immutable {
-          "" "";
-          default ", immutable";
-      }
-    '';
     services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
       forceSSL = true;
       enableACME = true;
-      extraConfig = ''
-        index index.php index.html /index.php$request_uri;
-          ${lib.strings.optionalString (config.services.nextcloud.nginx.recommendedHttpHeaders) ''
-            add_header Cache-Control                     "public, max-age=15778463$asset_immutable";
-            add_header X-Content-Type-Options nosniff;
-            add_header X-XSS-Protection "1; mode=block";
-            add_header X-Robots-Tag "noindex, nofollow";
-            add_header X-Download-Options noopen;
-            add_header X-Permitted-Cross-Domain-Policies none;
-            add_header X-Frame-Options sameorigin;
-            add_header Referrer-Policy no-referrer;
-          ''}
-          ${lib.strings.optionalString (config.services.nextcloud.https) ''
-            add_header Strict-Transport-Security "max-age=${toString config.services.nextcloud.nginx.hstsMaxAge}; includeSubDomains" always;
-          ''}
-      '';
       locations = {
         "~ \\.(?:css|js|mjs|svg|gif|png|jpg|jpeg|ico|wasm|tflite|map|html|ttf|bcmap|mp4|webm|ogg|flac)$".extraConfig = lib.mkForce ''
           try_files $uri /index.php$request_uri;
