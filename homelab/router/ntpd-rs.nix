b2e2dd546897a969f5303a06407d282c5fbfcd0c
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-05-28 18:36:38 +0100

    homelab: init router services

---
+++ homelab/router/ntpd-rs.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  config,
+  ...
+}:
+{
+  config = lib.mkIf config.homelab.services.router.ntpd-rs.enable {
+    services.ntpd-rs = {
+      enable = true;
+      useNetworkingTimeServers = false;
+      metrics.enable = true;
+      settings = {
+        source = [
+          {
+            mode = "pool";
+            address = "nixos.pool.ntp.org";
+            count = 4; 
+          }
+        ];
+        server = [
+          {
+            listen  = "127.0.0.1:123";
+          }
+          {
+            listen = "[::1]:123";
+          }
+        ];
+      };
+    };
+    networking.firewall.interfaces.${config.homelab.services.router.devices.lan}.allowedUDPPorts = [
+      123
+    ];
+  };
+}
