42c8feee523250f60e9406fc1209c25c4f07354d
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-05-29 12:18:56 +0100

    homelab: more progress on router

--- homelab/infra/hp-dl380p-g8-LFF/default.nix
+++ homelab/infra/hp-dl380p-g8-LFF/default.nix
@@ -1,5 +1,8 @@
 { ... }: {
   imports = [ ./hardware-configuration.nix ];
   networking.hostName = "hp-dl380p-g8-LFF";
-  homelab.enable = true;
+  homelab = {
+    enable = true;
+    router.enable = true;
+  };
 }
