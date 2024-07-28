eba46874b596a820310fdb80f9a13f6cc523a857
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-07-28 01:39:43 +0100

    workstations: fix mini build (didnt gate sourceControl properly)

--- workstations/programs/git.nix
+++ workstations/programs/git.nix
@@ -1,37 +1,44 @@
-{ pkgs, lib, config, ... }:
 {
-  home-manager.users.philip.programs.git = lib.mkIf config.workstation.declarativeHome {
-    enable = true;
-    userName = config.workstation.sourceControl.userName;
-    userEmail = "p.wilk@student.reading.ac.uk";
-    aliases = {
-      pl = "log --graph --abbrev-commit --decorate --stat";
-      dh = "diff HEAD";
-      dhp = "diff HEAD~";
-      push-fwl = "push --force-with-lease";
-    };
-    diff-so-fancy.enable = true;
-    signing = {
-      signByDefault = true;
-      key = "/home/philip/.ssh/gitKey";
-    };
-    extraConfig = {
-      core = {
-        editor = "hx";
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  config = lib.mkIf config.workstation.declarativeHome {
+    home-manager.users.philip.programs.git = lib.mkIf config.workstation.declarativeHome {
+      enable = true;
+      userName = config.workstation.sourceControl.userName;
+      userEmail = "p.wilk@student.reading.ac.uk";
+      aliases = {
+        pl = "log --graph --abbrev-commit --decorate --stat";
+        dh = "diff HEAD";
+        dhp = "diff HEAD~";
+        push-fwl = "push --force-with-lease";
       };
-      gpg = {
-        format = "ssh";
+      diff-so-fancy.enable = true;
+      signing = {
+        signByDefault = true;
+        key = "/home/philip/.ssh/gitKey";
       };
-      pull = {
-        rebase = true;
+      extraConfig = {
+        core = {
+          editor = "hx";
+        };
+        gpg = {
+          format = "ssh";
+        };
+        pull = {
+          rebase = true;
+        };
+        push = {
+          autoSetupRemote = true;
+        };
+        init = {
+          defaultBranch = "main";
+        };
+        rerere.enabled = true;
       };
-      push = {
-        autoSetupRemote = true;
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      rerere.enabled = true;
     };
   };
 }
--- workstations/programs/mercurial.nix
+++ workstations/programs/mercurial.nix
@@ -1,12 +1,10 @@
+{ config, lib, ... }:
 {
-  config,
-  lib,
-  ...
-}:
-{
- home-manager.users.philip.programs.mercurial = lib.mkIf config.workstation.declarativeHome {
-    enable = true;
-    userName = config.workstation.sourceControl.userName;
-    userEmail = config.workstation.sourceControl.userEmail;
+  config = lib.mkIf config.workstation.declarativeHome {
+    home-manager.users.philip.programs.mercurial = lib.mkIf config.workstation.declarativeHome {
+      enable = true;
+      userName = config.workstation.sourceControl.userName;
+      userEmail = config.workstation.sourceControl.userEmail;
+    };
   };
 }
