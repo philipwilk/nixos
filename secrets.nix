ab1959e67d7bc2471c24a0b7c49ffad85f6a5fd8
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-08-11 14:13:34 +0100

    flake: remove configurations for sold servers

--- secrets.nix
+++ secrets.nix
@@ -7,18 +7,8 @@ let
   ];
 
   nixos-thinkcentre-tiny = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB1WiWULvH38ludFzWf25wJ/k2oHcub8LH1rLujGPqot philip@nixos-thinkcentre-tiny";
-  hp-dl380p-g8-LFF = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINvP8XDrObooaEQF3vstla4XkRQJk7VdOA5kSidsnuJ/ philip@hp-dl380p-g8-LFF";
-  hp-dl380p-g8-sff-2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDmfy2zAyZ4Kk5QbVQ8fM19C1djXsab1Fe9hVrywW2xW philip@hp-dl380p-g8-sff-2";
-  hp-dl380p-g8-sff-3 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFOE/BZJYLIUcGzfrPjG3TciXKQrFhWrm6Imwkf+vRfO philip@hp-dl380p-g8-sff-3";
-  hp-dl380p-g8-sff-4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyHEr8Xgfvh/eLir8wWLabqSH7laWNrw7/Uo2MWT2NF philip@hp-dl380p-g8-sff-4";
-  hp-dl380p-g8-sff-5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPsb8pRSRiBZaPv/7VBYnPmSMjL15dzhRwZrbcOkJ2eb philip@hp-dl380p-g8-sff-5";
   servers = [
     nixos-thinkcentre-tiny
-    hp-dl380p-g8-LFF
-    hp-dl380p-g8-sff-2
-    hp-dl380p-g8-sff-3
-    hp-dl380p-g8-sff-4
-    hp-dl380p-g8-sff-5
   ];
 
   s = x: "secrets/${x}.age";
