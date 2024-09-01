4d3e7f1407a2c59a9d9c880401427ac581b9c718
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-09-01 20:08:18 +0100

    homelab: enable hercules and harmonia on itxserve for faster cache builds

--- homelab/infra/itxserve/default.nix
+++ homelab/infra/itxserve/default.nix
@@ -1,22 +1,21 @@
+{ config, ... }:
 {
-  config,
-  ...
-}:
-{
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
   networking.hostName = "itxserve"; # Define your hostname.
   networking.hostId = "9e09d7d7"; # needed for zfs
-  
+
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  homelab.services = {
-    nginx.enable = true;
-    nextcloud = {
-      enable = true;
-      domain = "cloud.${config.homelab.tld}";
+  homelab = {
+    services = {
+      nginx.enable = true;
+      nextcloud = {
+        enable = true;
+        domain = "cloud.${config.homelab.tld}";
+      };
+      harmonia.enable = true;
     };
+    nix.hercules-ci.enable = true;
   };
 }
