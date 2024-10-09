8ac952a33177fb7040ff0f7433b7773ada0eba86
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-10-09 09:09:47 +0100

    workstations: add lsp binds, enable auto pairs, use java-language-server instead of jdtls, fix tabs using spaces... et al

--- workstations/programs/kakoune/default.nix
+++ workstations/programs/kakoune/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, config, lib, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 let
   cfg = config.programs.kakoune;
 in
@@ -13,7 +18,7 @@ in
       To apply the color scheme use `programs.kakoune.config.colorScheme = "theme"`.
     '';
   };
-    
+
   config = lib.mkMerge [
     {
       xdg.configFile."kak-lsp/kak-lsp.toml".source = ./kak-lsp.toml;
@@ -26,6 +31,8 @@ in
             enableMouse = true;
             assistant = "cat";
           };
+          tabStop = 2;
+          indentWidth = 2;
           colorScheme = "catppuccin_latte";
           showWhitespace.enable = true;
           showMatching = true;
@@ -33,9 +40,56 @@ in
           wrapLines.enable = true;
         };
         extraConfig = ''
+          # Enable auto pairs
+          enable-auto-pairs
+
+          # Smarttab
+          hook global BufOpenFile .* expandtab
+          hook global BufNewFile  .* expandtab
+
+          hook global BufCreate .* %{
+            editorconfig-load
+            autoconfigtab
+          }
+
+          hook global ModuleLoaded smarttab %{
+            set-option global softtabstop ${toString config.programs.kakoune.config.tabStop}
+          }
+
           # LSP
           eval %sh{kak-lsp --kakoune -s $kak_session}
           lsp-enable
+
+          # # Format file on write
+          hook global BufSetOption filetype=* %{
+            hook buffer BufWritePre .* lsp-formatting-sync
+          }
+
+          lsp-inlay-hints-enable global
+
+          # Bind lsp tab completion and tab indenting
+          map global insert <tab> <c-n>
+          map global insert <s-tab> <c-p>
+
+          hook global InsertCompletionShow .* %{
+            map global insert <tab> <c-n>
+            map global insert <s-tab> <c-p>
+          }
+          hook global InsertCompletionHide .* %{
+            unmap global insert <tab>
+            unmap global insert <s-tab>
+          }
+
+          map global user l %{:enter-user-mode lsp<ret>} -docstring "LSP mode"
+
+          map global insert <tab> '<a-;>:try lsp-snippets-select-next-placeholders catch %{ execute-keys -with-hooks <lt>tab> }<ret>' -docstring 'Select next snippet placeholder'
+
+          map global object a '<a-semicolon>lsp-object<ret>' -docstring 'LSP any symbol'
+          map global object <a-a> '<a-semicolon>lsp-object<ret>' -docstring 'LSP any symbol'
+          map global object f '<a-semicolon>lsp-object Function Method<ret>' -docstring 'LSP function or method'
+          map global object t '<a-semicolon>lsp-object Class Interface Struct<ret>' -docstring 'LSP class interface or struct'
+          map global object d '<a-semicolon>lsp-diagnostic-object --include-warnings<ret>' -docstring 'LSP errors and warnings'
+          map global object D '<a-semicolon>lsp-diagnostic-object<ret>' -docstring 'LSP errors'
 
           # Kakboard
           hook global WinCreate .* %{ kakboard-enable }
@@ -51,13 +105,13 @@ in
           auto-pairs-kak
           active-window-kak
           kakoune-lsp
+          smarttab-kak
         ];
         colorSchemePackage = pkgs.kakounePlugins.kakoune-catppuccin;
       };
     }
     (lib.mkIf (cfg.colorSchemePackage != null) {
-      xdg.configFile."kak/colors/${cfg.colorSchemePackage.name}".source =
-        cfg.colorSchemePackage;
+      xdg.configFile."kak/colors/${cfg.colorSchemePackage.name}".source = cfg.colorSchemePackage;
     })
   ];
 }
