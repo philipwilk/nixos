4265ddae35b69fa682a2f3764da71e4e07e8ee92
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-12-22 18:21:36 +0000

    flake: use one over quad9 because ive had some reliability issues

--- workstations/default.nix
+++ workstations/default.nix
@@ -152,8 +152,8 @@ in
           dnsovertls = "true";
           domains = [ "~." ];
           extraConfig = ''
-            DNS=9.9.9.9#dns.quad9.net [2620:fe::fe]#dns.quad9.net
-            FallbackDNS=1.1.1.1#cloudflare-dns.com [2606:4700:4700::1111]#cloudflare-dns.com
+            DNS=1.1.1.1#cloudflare-dns.com [2606:4700:4700::1111]#cloudflare-dns.com
+            FallbackDNS=9.9.9.9#dns.quad9.net [2620:fe::fe]#dns.quad9.net
           '';
         };
       };
