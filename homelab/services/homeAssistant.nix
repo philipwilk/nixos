6d74a4a1f88fcff562d867fc4db6ecec13de2204
Philip Wilk <p.wilk@student.reading.ac.uk>
2024-12-23 02:59:42 +0000

    home assistant: try use thread only

--- homelab/services/homeAssistant.nix
+++ homelab/services/homeAssistant.nix
@@ -4,6 +4,11 @@
   config,
   ...
 }:
+let
+  otbrApiPort = 8081;
+  otbrPort = 8086;
+  dongleDevice = "/dev/ttyUSB0";
+in
 {
   options.homelab.services.homeAssistant.enable = lib.mkEnableOption "the home assistant config";
 
@@ -34,20 +39,73 @@
         # Includes dependencies for a basic setup
         # https://www.home-assistant.io/integrations/default_config/
         default_config = { };
-        # for zigbee OTA
-        zha.zigpy_config.ota.z2m_remote_index = "https://raw.githubusercontent.com/Koenkk/zigbee-OTA/master/index.json";
-      };
 
-      # for proxying
-      config.http = {
-        server_host = "::1";
-        trusted_proxies = [ "::1" ];
-        use_x_forwarded_for = true;
+        # to use postgres
+        recorder.db_url = "postgresql://@/hass";
       };
-
-      # For postgres use
       extraPackages = ps: with ps; [ psycopg2 ];
-      config.recorder.db_url = "postgresql://@/hass";
+    };
+
+    # for silabs multiprotocol
+    # https://hub.docker.com/r/b2un0/silabs-multipan-docker
+    networking.firewall.allowedTCPPorts = [
+      otbrPort
+    ];
+    #virtualisation.oci-containers.containers."silabs-multipan" = {
+    #  image = "docker.io/b2un0/silabs-multipan-docker:2.4.5";
+    #  environment = {
+    #    BACKBONE_IF = config.homelab.net.lan;
+    #    DEVICE = dongleDevice;
+    #    FLOW_CONTROL = "false";
+    #  };
+    #  devices = [
+    #    "${dongleDevice}:${dongleDevice}"
+    #    "/dev/net/tun"
+    #  ];
+    #  capabilities = {
+    #    CAP_NET_ADMIN = true;
+    #    CAP_NET_RAW = true;
+    #  };
+    #  extraOptions = [
+    #    "--network=host"
+    #  ];
+    #  ports = [
+    #    "${toString otbrPort}:${toString otbrPort}"
+    #    "${toString otbrApiPort}:${toString otbrApiPort}"
+    #  ];
+    #  volumes = [ "${config.homelab.stateDir}/multipan:/data:Z" ];
+    #};
+    #
+    virtualisation.oci-containers.containers."otbr" = {
+      image = "docker.io/openthread/otbr:latest";
+      environment = {
+        INFRA_IF_NAME = config.homelab.net.lan;
+      };
+      devices = [
+        dongleDevice
+      ];
+      capabilities = {
+        CAP_NET_ADMIN = true;
+        CAP_NET_RAW = true;
+      };
+      privileged = true;
+      cmd = [
+        "--radio-url=spinel+hdlc+uart://${dongleDevice}"
+      ];
+      ports = [
+        "8081:8081"
+        "8086:80"
+      ];
+    };
+
+    services.matter-server.enable = true; # needs stateDir patch
+    hardware.bluetooth.enable = true;
+
+    # for proxying
+    services.home-assistant.config.http = {
+      server_host = "::1";
+      trusted_proxies = [ "::1" ];
+      use_x_forwarded_for = true;
     };
 
     services.postgresql = {
