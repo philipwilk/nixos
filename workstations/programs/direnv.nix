368b18c61e40915a8dac4b7380c920ccdb1654e9
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-02-11 13:44:40 +0000

    workstations/direnv: remove variable vomit

--- workstations/programs/direnv.nix
+++ workstations/programs/direnv.nix
@@ -5,6 +5,13 @@
       enable = true;
       enableNushellIntegration = true;
       nix-direnv.enable = true;
+      config = {
+        global = {
+          warn_timeout = "0s";
+          hide_env_diff = true;
+          load_dotenv = true;
+        };
+      };
     };
   };
 }
