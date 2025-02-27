1172016b9265eb2f7805e99c164d931575b483a6
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-02-27 16:45:59 +0000

    homelab/jupyterlab: add pylsp, jupyterlab-git, git

--- homelab/services/jupyter.nix
+++ homelab/services/jupyter.nix
@@ -42,12 +42,36 @@ in
           c.GenericOAuthenticator.allowed_groups = { 'idm_all_persons@testing-idm.fogbox.uk' }
           c.GenericOAuthenticator.admin_groups = { 'idm_people_admins@testing-idm.fogbox.uk' }
         '';
-      jupyterhubEnv = pkgs.python3.withPackages (
-        p: with p; [
-          jupyterhub
-          jupyterhub-systemdspawner
-          oauthenticator
-        ]
+      jupyterhubEnv = (
+        pkgs.buildEnv {
+          name = "jupterhub-env";
+          paths = with pkgs; [
+            (python3.withPackages (
+              p: with p; [
+                jupyterhub
+                jupyterhub-systemdspawner
+                oauthenticator
+              ]
+            ))
+            git
+          ];
+        }
+      );
+      jupyterlabEnv = (
+        pkgs.buildEnv {
+          name = "jupyterlab-env";
+          paths = with pkgs; [
+            (pkgs.python3.withPackages (
+              p: with p; [
+                jupyterhub
+                jupyterlab
+                python-lsp-server
+                jupyterlab-git
+              ]
+            ))
+            git
+          ];
+        }
       );
       kernels.python3 =
         let
