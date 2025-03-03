c1618c82db4f3ab28de40d2a8aa5b254369268c2
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-03-03 17:20:29 +0000

    flake/lanzaboote: move all secureboot pkis to new location

--- configs/boot/lanzaboote.nix
+++ configs/boot/lanzaboote.nix
@@ -11,7 +11,7 @@
     loader.systemd-boot.enable = lib.mkForce false;
     lanzaboote = {
       enable = true;
-      pkiBundle = "/etc/secureboot";
+      pkiBundle = "/var/lib/sbctl";
     };
   };
 }
