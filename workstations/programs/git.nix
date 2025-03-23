b57a1666ce194878823e59ba2e9631f68cb76520
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-03-23 17:41:57 +0000

    workstation/programs/git: add more aliases

--- workstations/programs/git.nix
+++ workstations/programs/git.nix
@@ -6,38 +6,54 @@
 }:
 {
   config = lib.mkIf config.workstation.declarativeHome {
-    home-manager.users.philip.programs.git = lib.mkIf config.workstation.declarativeHome {
-      enable = true;
-      userName = config.workstation.sourceControl.userName;
-      userEmail = "p.wilk@student.reading.ac.uk";
-      aliases = {
-        pl = "log --graph --abbrev-commit --decorate --stat";
-        dh = "diff HEAD";
-        dhp = "diff HEAD~";
-        push-fwl = "push --force-with-lease";
+    home-manager.users.philip = lib.mkIf config.workstation.declarativeHome {
+      programs.git = {
+        enable = true;
+        userName = config.workstation.sourceControl.userName;
+        userEmail = "p.wilk@student.reading.ac.uk";
+        aliases = {
+          pl = "log --graph --abbrev-commit --decorate --stat";
+          dh = "diff HEAD";
+          dhp = "diff HEAD~";
+          push-fwl = "push --force-with-lease";
+          rhp = "reset HEAD~";
+          rhph = "reset HEAD~ --hard";
+          rhh = "reset HEAD --hard";
+        };
+        diff-so-fancy.enable = true;
+        signing = {
+          signByDefault = true;
+          key = "/home/philip/.ssh/gitKey";
+        };
+        extraConfig = {
+          core = {
+            editor = "hx";
+          };
+          gpg = {
+            format = "ssh";
+          };
+          pull = {
+            rebase = true;
+          };
+          push = {
+            autoSetupRemote = true;
+          };
+          init = {
+            defaultBranch = "main";
+          };
+          rerere.enabled = true;
+        };
       };
-      diff-so-fancy.enable = true;
-      signing = {
-        signByDefault = true;
-        key = "/home/philip/.ssh/gitKey";
+      programs.gh = {
+        enable = true;
+        gitCredentialHelper.enable = false;
+        settings = {
+          git_protocol = "ssh";
+        };
       };
-      extraConfig = {
-        core = {
-          editor = "hx";
-        };
-        gpg = {
-          format = "ssh";
-        };
-        pull = {
-          rebase = true;
-        };
-        push = {
-          autoSetupRemote = true;
-        };
-        init = {
-          defaultBranch = "main";
-        };
-        rerere.enabled = true;
+      programs.gh-dash = {
+        enable = true;
+        settings = { };
       };
     };
   };
