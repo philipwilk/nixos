15ca701a460f161997de8ceafbb6f5c2dde1525c
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-05-22 01:53:20 +0100

    flake/patches: drop upstreamed imbalanced-learn and hddfancontrol. move all patches in-tree

--- configs/boot/systemd.nix
+++ configs/boot/systemd.nix
@@ -5,6 +5,19 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    kernelPackages = lib.mkDefault (
+      pkgs.linuxPackagesFor (
+        pkgs.linuxKernel.kernels.linux_6_14.override {
+          argsOverride = rec {
+            src = pkgs.fetchurl {
+              url = "mirror://kernel/linux/kernel/v${lib.versions.major version}.x/linux-${version}.tar.xz";
+              hash = "sha256-KCB+xSu+qjUHAQrv+UT0QvfZ8isoa3nK9F7G3xsk9Ak=";
+            };
+            version = "6.14.5";
+            modDirVersion = "6.14.5";
+          };
+        }
+      )
+    );
   };
 }
