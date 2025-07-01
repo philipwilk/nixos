8fc77ff93f490373264ccfa0bddbb10223d7c200
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-07-01 13:10:25 +0100

    workstations/systems: disable dnssec

--- workstations/default.nix
+++ workstations/default.nix
@@ -182,7 +182,6 @@ in
         };
         resolved = {
           enable = true;
-          dnssec = "true";
           dnsovertls = "opportunistic";
           extraConfig = ''
             FallbackDNS=9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net [2620:fe::fe]#dns.quad9.net [2620:fe::9]#dns.quad9.net
