847fd5c64ec58be418f0f74d66bd8dc4c198c29f
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-09-07 11:27:22 +0100

    flake/nixpkgs/nix: update http-binary-cache-store patch

--- overlays/nix/default.nix
+++ overlays/nix/default.nix
@@ -6,7 +6,10 @@
           finalScope: prevScope: {
             aws-sdk-cpp = null;
             withAWS = false;
-            patches = [ ./0001-libstore-allow-http-binary-cache-store-to-soft-error.patch ];
+            patches = [
+              ./0001-bugfix-3514-build-fails-when-cache-offline.patch
+              ./0002-feat-13301-fallback-from-failing-substituters-treat-.patch
+            ];
           }
         )
       );
