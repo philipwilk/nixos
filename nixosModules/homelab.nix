b3adcbd351079d1fc2687ad73831ae582058d17e
Philip Wilk <p.wilk@student.reading.ac.uk>
2025-10-20 20:02:05 +0100

    flake: remove upstreamed patches

--- nixosModules/homelab.nix
+++ nixosModules/homelab.nix
@@ -7,9 +7,6 @@
     ./homelab
 
     # replaced modules
-    ./replace/services/monitoring/ups.nix
     ./replace/services/monitoring/prometheus/exporters.nix
-    ./replace/services/hardware/hddfancontrol.nix
-    ./replace/hardware/sensor/hddtemp.nix
   ];
 }
