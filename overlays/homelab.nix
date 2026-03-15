e6d9fef64bab05f700a926173769dab80bb2b664
Philip Wilk <p.wilk@student.reading.ac.uk>
2026-03-15 11:40:44 +0000

    homelab/jupyterlab: fix mlxtend: remove redundant patch
    track nixpkgs fix: https://github.com/NixOS/nixpkgs/pull/500054

--- overlays/homelab.nix
+++ overlays/homelab.nix
@@ -5,5 +5,18 @@
   nixpkgs.overlays = [
     # add cpu monitoring option
     (import ./hddfancontrol)
+
+    (final: prev: {
+      python312 = prev.python312.override {
+        packageOverrides = pFinal: pPrev: {
+          mlxtend = prev.python312.pkgs.callPackage ./mlxtend { };
+        };
+      };
+      python313 = prev.python313.override {
+        packageOverrides = pFinal: pPrev: {
+          mlxtend = prev.python313.pkgs.callPackage ./mlxtend { };
+        };
+      };
+    })
   ];
 }
