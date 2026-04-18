a55b9ffcb25249e80c526707edd5255c0050d05e
Philip Wilk <p.wilk@student.reading.ac.uk>
2026-04-18 11:35:44 +0100

    cartopy: revert ef102bed75b9625eebe54d8aa9f82fb17978d8e5

--- overlays/homelab.nix
+++ overlays/homelab.nix
@@ -22,6 +22,18 @@
       };
     })
 
+    (final: prev: {
+      proj = prev.proj.overrideAttrs {
+        version = "9.7.1";
+        src = prev.fetchFromGitHub {
+          owner = "OSGeo";
+          repo = "PROJ";
+          tag = "9.7.1";
+          hash = "sha256-xXtqbLPS2Hu9gC06b72HDjnNRh4m0ism97hP8FFYOMo=";
+        };
+      };
+    })
+
     (import ./searxng)
   ];
 }
